72948191abe5198278b8a670cb29080ebb14ca18
limao <limao@qunhemail.com>
2023-03-29 14:54:34 +0800

    reset

--- .umirc.ts
+++ .umirc.ts
@@ -7,27 +7,17 @@ export default defineConfig({
   initialState: {},
   request: {},
   layout: {
-    title: '@umijs/max',
+    title: 'collision',
   },
   routes: [
     {
       path: '/',
-      redirect: '/home',
+      redirect: '/collision',
     },
     {
-      name: '首页',
-      path: '/home',
-      component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-        name: ' CRUD 示例',
-        path: '/table',
-        component: './Table',
+      name: 'collision',
+      path: '/collision',
+      component: './Collision',
     },
   ],
   npmClient: 'yarn',
